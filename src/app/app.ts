17189296c1190d1e1a0a389a7d0697187c115957
Dwayne <dwayne@reddingdesigns.com>
2025-06-21 14:04:54 -0400

    set up navbar component and add to app

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {NavbarComponent} from './shared/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [
+    RouterOutlet,
+    NavbarComponent
+  ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
