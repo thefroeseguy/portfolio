ea48bd8069ad3accf704127bc4ba73f7f99b8816
Dwayne <dwayne@reddingdesigns.com>
2025-06-26 07:26:41 -0400

    update about card text

--- src/app/components/about/about.ts
+++ src/app/components/about/about.ts
@@ -9,5 +9,5 @@ import { Component } from '@angular/core';
 })
 export class About {
   title = 'A little about me';
-  description: string = 'A self-taught developer, I have earned a reputation as a quick learner and problem solver. I am Brett\'s number one fan. Outside of work, I enjoy playing guitar and ukulele as well as volunteering as a youth leader at my church. I am married together we have three beautiful daughters';
+  description: string = 'A self-taught developer, I have earned a reputation as a quick learner and problem solver. I am Brett\'s number one fan. Outside of work, I enjoy playing guitar and ukulele as well as volunteering as a youth leader at my church. I am married and together we have three beautiful daughters.';
 }
