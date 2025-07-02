23bd3b9235b50b5ceeeb9bc1754bdc76334e97f6
Dwayne <dwayne@reddingdesigns.com>
2025-07-01 21:42:18 -0400

    add experience card component

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { Hero } from '../../components/hero/hero';
 import {About} from '../../components/about/about';
+import {Experience} from '../../components/experience/experience';
 
 @Component({
   selector: 'app-home',
   imports: [
     Hero,
-    About
+    About,
+    Experience
   ],
   templateUrl: './home.html',
   standalone: true,
